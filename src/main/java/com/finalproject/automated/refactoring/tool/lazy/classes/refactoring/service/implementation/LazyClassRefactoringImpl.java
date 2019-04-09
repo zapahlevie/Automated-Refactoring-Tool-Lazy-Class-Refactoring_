e0260da9889294ec2120a466dd7fb19156734f3e
zapahlevie <zapahlevie@gmail.com>
2019-04-09 09:26:46 +0700

    first commit

---
+++ src/main/java/com/finalproject/automated/refactoring/tool/lazy/classes/refactoring/service/implementation/LazyClassRefactoringImpl.java
@@ -0,0 +1,18 @@
+package com.finalproject.automated.refactoring.tool.lazy.classes.refactoring.service.implementation;
+
+import com.finalproject.automated.refactoring.tool.lazy.classes.refactoring.service.LazyClassRefactoring;
+import com.finalproject.automated.refactoring.tool.model.ClassModel;
+
+import java.util.List;
+
+public class LazyClassRefactoringImpl implements LazyClassRefactoring {
+    @Override
+    public ClassModel refactor(ClassModel targetClass, ClassModel lazyClass) {
+        return null;
+    }
+
+    @Override
+    public List<ClassModel> refactor(List<ClassModel> targetClasses, List<ClassModel> lazyClasses) {
+        return null;
+    }
+}
